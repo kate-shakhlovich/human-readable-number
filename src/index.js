32781b39ee96ff6ef6ce94605812d876fe4aaa38
Kate Shakhlovich <kate.shakhlovich@gmail.com>
2020-03-17 00:53:44 +0300

    feat: human readable number

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-module.exports = function toReadable (number) {
-  return [getHundreds(number), getDozens(number), getSingle(number)].filter(x => x).join(" ");
+module.exports = function toReadable(number) {
+    return [getHundreds(number), getDozens(number), getSingle(number)].filter(x => x).join(" ");
 }
 
 const getHundreds = (number) => {
-    // TODO
+    if (number < 100) return '';
+    const hundreds = number / 100;
+    return oneDigit[Math.floor(hundreds)] + ' hundred';
 }
 
 const getDozens = (number) => {
-    // TODO
+    if (number < 10) return '';
+    const dozens = number % 100;
+    if (dozens > 19) {
+        const dozenCount = Math.floor(dozens / 10);
+        return twoDigit[dozenCount];
+    }
+    return teens[dozens];
 }
 
 const getSingle = (number) => {
-    // TODO
+    if (number === 0) return oneDigit[0];
+    const single = number % 10;
+    const dozens = number % 100;
+    return single === 0 || (dozens > 10 && dozens < 20) ? '' : oneDigit[single];
 }
 
 const oneDigit = {
@@ -28,7 +39,6 @@ const oneDigit = {
 };
 
 const twoDigit = {
-    1: 'ten',
     2: 'twenty',
     3: 'thirty',
     4: 'forty',
@@ -40,13 +50,14 @@ const twoDigit = {
 };
 
 const teens = {
-    1: "eleven",
-    2: "twelve",
-    3: "thirteen",
-    4: "fourteen",
-    5: "fifteen",
-    6: "sixteen",
-    7: "seventeen",
-    8: "eighteen",
-    9: "nineteen"
-}
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen"
+};
